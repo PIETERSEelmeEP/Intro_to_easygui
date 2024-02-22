5bf90fc7484ce59d9bad5f3cf01da6155e0c572b
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-23 11:28:46 +1300

    Update favourite_places_v1.py

--- favourite_places_v1.py
+++ favourite_places_v1.py
@@ -3,6 +3,8 @@ import easygui
 favourite_places = ["Italy", "France", "England"]
 
 easygui.msgbox(f"The place I would most like to visit is "
-               f"{favourite_places[0]}")
-easygui.msgbox(f"My second choice would be {favourite_places[1]}")
-easygui.msgbox(f"My third choice for places to visit is {favourite_places[2]}")
+               f"{favourite_places[0]}", title="Location One")
+easygui.msgbox(f"My second choice would be {favourite_places[1]}",
+               title="Location Two")
+easygui.msgbox(f"My third choice for places to visit is {favourite_places[2]}",
+               title="Location Three")
