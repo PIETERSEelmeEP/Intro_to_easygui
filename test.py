684ee87012bf5c2f9f902263a59cd36a813992e1
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-23 11:17:58 +1300

    Update test.py

--- test.py
+++ test.py
@@ -13,15 +13,15 @@ letter = random.choice(friend)
 print(letter)
 print(friend[3])
 
-# name = easygui.enterbox("Hey, What's your name?")
-# print(name)
-#
-# age = easygui.integerbox("How old are you?")
-# carry_on = easygui.buttonbox("Do you want to continue?",
-#                              choices=["Yes", "No", "Don't care"],
-#                              title="Continue")
-#
-# welcome = easygui.msgbox("Welcome to Easygui", title="Welcome")
-#
-# gender = easygui.buttonbox("Choose gender", title="Gender",
-#                            choices=["Male", "Female", "Other"])
+name = easygui.enterbox("Hey, What's your name?")
+print(name)
+
+age = easygui.integerbox("How old are you?")
+carry_on = easygui.buttonbox("Do you want to continue?",
+                             choices=["Yes", "No", "Don't care"],
+                             title="Continue")
+
+welcome = easygui.msgbox("Welcome to Easygui", title="Welcome")
+
+gender = easygui.buttonbox("Choose gender", title="Gender",
+                           choices=["Male", "Female", "Other"])
